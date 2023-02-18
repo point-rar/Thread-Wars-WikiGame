8777f8efb93c99e42437ce2d3294670a4d1db95b
Шахов Александр <al.shakhov@tinkoff.ru>
2023-02-19 00:39:27 +0300

    Init commit kotlin

---
+++ wiki-game-coroutines/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.8.10"
+}
+
+group = "point.rar"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
---
+++ wiki-game-coroutines/src/main/java/point/rar/Main.kt
@@ -0,0 +1,6 @@
+package point.rar
+
+
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
